02c6013ffa5382e106f6fbc3656a83316510ea0a
Meg Liu <margs.liu@gmail.com>
2026-05-29 17:57:08 -0700

    Account for normally open/closed button types
    The first button I tested with appears to be a normally closed button, which explains why I had to check for HIGH in my original isMomentaryButtonPressed.
    
    I confirmed the difference independently with my own NO switch.

--- control_panel_perfex_mod/control_panel_perfex_mod.ino
+++ control_panel_perfex_mod/control_panel_perfex_mod.ino
@@ -16,12 +16,16 @@ void setup() {
   pinMode(PERFEX_INTERCOM_BUTTON_PIN, INPUT_PULLUP); 
 }
 
-bool isMomentaryButtonPressed(int buttonPinState) {
+bool isNormallyOpenButtonPressed(int buttonPinState) {
+  return !!(digitalRead(buttonPinState) == LOW);
+}
+
+bool isNormallyClosedButtonPressed(int buttonPinState) {
   return !!(digitalRead(buttonPinState) == HIGH);
 }
 
 void loop() {
-  if (isMomentaryButtonPressed(PERFEX_INTERCOM_BUTTON_PIN)) {
+  if (isNormallyOpenButtonPressed(PERFEX_INTERCOM_BUTTON_PIN)) {
     onboardRGBLED.setPixelColor(0, onboardRGBLED.Color(255, 255, 255));
   } else {
     onboardRGBLED.setPixelColor(0, onboardRGBLED.Color(0, 0, 0));
